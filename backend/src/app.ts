b84edff43cdda3334945f6dbdbd3cb642ec4549b
Acinate <Acinate@hotmail.com>
2019-09-22 18:07:32 -0400

    added new authorized domains

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:8080"
+    origin: ["http://localhost:8080", "159.65.160.232", "http://jefthimi.net/"]
   })
 );
 
