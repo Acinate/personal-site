9b0fd98431546cfa0517e4b3ffdcbfd0d81ca68c
Acinate <acinate@hotmail.com>
2019-12-25 02:24:52 -0500

    refactored navbar component

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import './Navbar.scss';
+import {Link} from 'react-router-dom';
 
 export default function Navbar() {
   return (
