6305adcc9f2a22b594a99feefa647d7b47d70897
Acinate <acinate@hotmail.com>
2020-01-08 15:05:44 -0500

    fixed api url

--- frontend/src/api/blog.ts
+++ frontend/src/api/blog.ts
@@ -1,9 +1,10 @@
 import axios, {AxiosResponse} from 'axios';
 import Post from "../../../backend/src/models/post";
+import {baseUrl} from "./_api";
 
 export const getPosts = async (): Promise<Post> => {
     return new Promise<Post>((resolve, reject) => {
-        axios.get('http://localhost:3000/blog').then((response: AxiosResponse) => {
+        axios.get(`${baseUrl}/blog`).then((response: AxiosResponse) => {
             resolve(response.data);
         }).catch((error) => {
             reject(error)
