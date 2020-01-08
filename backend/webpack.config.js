894966dc834b6e005d834b021bcff9bd3cc3c0af
Acinate <acinate@hotmail.com>
2020-01-08 15:40:40 -0500

    added markdown loader

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,42 +1,53 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  target: "node",
-  entry: {
-    server: ["./src/server.ts"]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-    publicPath: "/"
-  },
-  devtool: "source-map",
-  optimization: {
-    minimize: true
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new Dotenv({ path: ".env", safe: false, silent: false })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".ts"],
-    modules: [path.resolve("./node_modules"), path.resolve("./src")]
-  }
+    mode: "production",
+    target: "node",
+    entry: {
+        server: ["./src/server.ts"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].bundle.js",
+        publicPath: "/"
+    },
+    devtool: "source-map",
+    optimization: {
+        minimize: true
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new Dotenv({path: ".env", safe: false, silent: false})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.md$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    },
+                    {
+                        loader: "markdown-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+        modules: [path.resolve("./node_modules"), path.resolve("./src")]
+    }
 };
--- frontend/src/api/_api.ts
+++ frontend/src/api/_api.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = "http://" + window.location.hostname + ":3000"; // for development
-export const baseUrl = "https://" + window.location.hostname + "/api"; // for production
+export const baseUrl = "http://" + window.location.hostname + ":3000"; // for development
+// export const baseUrl = "https://" + window.location.hostname + "/api"; // for production
